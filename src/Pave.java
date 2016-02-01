f8e516ded332a210e552d3eac416c4364decdfbf
Jimmy-Jones <alexis1.dahan@gmail.com>
2016-02-01 12:03:14 +0100

    Ajout du programme Pave.java

--- src/Pave.java
+++ src/Pave.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
-
-public class Pave {
-    public static void main(String ... args) {
-
-	//1. Demander chaque longueur grâce au Scanner
-	Scanner scan = new Scanner(System.in);
-	
-    }
+public class Pave{
+        public static void main (String [] args) {
+           float Longueur=askValue("Longueur");
+           float Largeur= askValue("Largeur");
+           float Hauteur= askValue("Hauteur");
+  
+           float Volume=Hauteur*Largeur*Longueur;
+  
+           System.out.println("Le volume du Pave est égal à"+Volume);
 }
+public static float askValue(String name){
+       
+           System.out.print("Entrez la " +name);
+Scanner scan=new Scanner(System.in);
+return scan.nextFloat();
+}}
